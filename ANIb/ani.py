d53804021127bb753f39facb31fc3a11a52bc550
Yoann Dufresne <yoann.dufresne0@gmail.com>
2020-06-22 16:26:02 +0200

    update from all vs all to queries vs targets

--- ANIb/ani.py
+++ ANIb/ani.py
@@ -7,6 +7,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='ANI all vs all computation')
     parser.add_argument('genome_list', type=str, help='A file containing all the genome to compare. One genome per line.')
     parser.add_argument('--cores', '-c', type=int, default=8, help='The number of cores to use')
+    parser.add_argument('--queries', '-q', help='A file list containing the names of the genomes to compare with the whole genome list. If not specified, all vs all is performed.')
     
     args = parser.parse_args()
 
@@ -38,24 +39,11 @@ def compute_ANI(genome1, genome2):
 
     return ANI
 
-def compute_all_ani(lst):
-    for idx, file in enumerate(lst):
-        for file2 in lst[idx+1:]:
-            ANI = compute_ANI(file, file2)
-            out1 = file.split('/')[-1]
-            out2 = file2.split('/')[-1]
-            print(f"{out1}\t{out2}\t{ANI}")
-
-def compute_parallel_ani(lst, cores):
-    pairs = []
-    for idx, file in enumerate(lst):
-        for file2 in lst[idx+1:]:
-            pairs.append((file, file2))
-
+def compute_parallel_ani(file_pairs, cores):
     p = Pool(cores)
-    results = p.starmap(compute_ANI, pairs)
+    results = p.starmap(compute_ANI, file_pairs)
 
-    for idx, pair in enumerate(pairs):
+    for idx, pair in enumerate(file_pairs):
         out1 = pair[0].split('/')[-1]
         out2 = pair[1].split('/')[-1]
         print(f"{out1}\t{out2}\t{results[idx]}")
@@ -63,7 +51,12 @@ def compute_parallel_ani(lst, cores):
 def main():
     args = parse_args()
     f_lst = parse_list(args.genome_list)
-    compute_parallel_ani(f_lst, args.cores)
+    pairs = []
+    if args.queries is None:
+        compute_parallel_ani(combinations(f_lst,2), args.cores)
+    else:
+        q_lst = parse_list(args.queries)
+        compute_parallel_ani(product(f_lst, q_lst), args.cores)
 
 
 if __name__ == "__main__":
